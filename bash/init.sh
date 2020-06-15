01e149708791194ecb1bebdbc10ed25e29958f9f
Abdiel Brilhante <abdiel@DESKTOP-SB0SA81.localdomain>
2020-06-15 16:05:32 -0300

    Fix wsl init and ps config

--- bash/init.sh
+++ bash/init.sh
@@ -3,7 +3,7 @@ sudo sh -c 'echo "[network]\ngenerateResolvConf = false" > /etc/wsl.conf'
 
 sudo rm -f /etc/resolv.conf
 sudo touch /etc/resolv.conf
-sudo sh -c 'echo "nameserver 8.8.8.8" > /etc/wsl.conf'
+sudo sh -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 
 sudo apt update
 sudo apt upgrade
